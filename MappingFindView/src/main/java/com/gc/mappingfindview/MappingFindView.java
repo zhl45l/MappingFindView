f014a647861983cb5b4fdb4a5666d40619b808ac
zhl45l <zhl45l@163.com>
2023-06-27 16:31:24 +0800

    Revert "对activity，自定义view，fragment以及 adapter的viewholder进行映射支持"

--- MappingFindView/src/main/java/com/gc/mappingfindview/MappingFindView.java
+++ MappingFindView/src/main/java/com/gc/mappingfindview/MappingFindView.java
@@ -1,20 +1,17 @@
 package com.gc.mappingfindview;
 
-import android.util.Log;
-
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 /**
- * Activity、Fragment、自定义View，Adapter中的所有属性，只要是View的子类的：，
- * 都会尝试去根据  属性名 作为 资源id，来加载资源，并赋值给该字段。
- * 如 属性 TextView tv_userName， 将尝试 findviewbyid(R.id.tv_userName)
- * 如果找到View则将自动赋值给 tv_userName;
+ * Activity中的所有属性，只要是View的子类的：
+ * 都会尝试去根据  属性名 作为 资源id，来加载资源，并赋值给改字段。
+ * 如 属性 TextView tv_userName， 将尝试 findviewbyid(R.id.tv_userName)如果找到View则将自动赋值给 tv_userName;
  */
 public class MappingFindView {
     private static String TAG = "MappingFindView";
-    private static String pkgName;
+    private static String pckName;
 
     /**
      * 映射 Activity中所有view子类的属性
@@ -26,25 +23,39 @@ public class MappingFindView {
     public static void mappingActivity(Object object) {
         assert Thread.currentThread().getName().equals("main");
         assert object != null;
-        mappingFindView(object, object, object);
+        if (pckName == null || pckName.equals("")) {
+            try {
+                pckName = getPackName(object);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        assert (pckName != null && !pckName.equals(""));
+
+        mappingFindView(object);
     }
 
     /**
      * 映射一个自定义View，将其中所有的字段和 已存在的同名布局 绑定
      * 必须主线程中调用，确保方法参数 都不为null
      *
-     * @param view 自定义view根布局
+     * @param content Content
+     * @param view    自定义view根布局
      */
-    public static void mappingView(Object view) {
+    public static void mappingView(Object content, Object view) {
         assert Thread.currentThread().getName().equals("main");
+        assert content != null;
         assert view != null;
-        Object context = null;
-        try {
-            context = getMethod(view.getClass(), "getContext").invoke(view);
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (pckName == null || pckName.equals("")) {
+            try {
+                pckName = getPackName(content.getClass().getMethod("getPackageName").invoke(content));
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-        mappingFindView(context, view, view);
+        assert (pckName != null && !pckName.equals(""));
+        mappingFindView(view, view);
     }
 
     /**
@@ -52,51 +63,35 @@ public class MappingFindView {
      * 目前需使用：androidx.fragment.app报下的 fragment
      * 必须主线程中调用，确保方法参数 都不为null
      *
-     * @param fragment fragment
-     * @param view     自定义view根布局
+     * @param target fragment
+     * @param source 自定义view根布局
      */
-    public static void mappingFragment(Object fragment, Object view) {
+    public static void mappingFragment(Object target, Object source) {
         assert Thread.currentThread().getName().equals("main");
-        assert fragment != null;
-        assert view != null;
-        Object context = null;
-        try {
-            context = getMethod(fragment.getClass(), "getContext").invoke(fragment);
-        } catch (Exception e) {
-            e.printStackTrace();
+        assert target != null;
+        assert source != null;
+        if (pckName == null || pckName.equals("")) {
+            try {
+                if (isFragment(target.getClass())) { // fragment
+                    pckName = getPackName(target.getClass().getMethod("getActivity").invoke(target));
+                } else { // view
+                    pckName = getPackName(target.getClass().getMethod("getPackageName").invoke(target));
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-        mappingFindView(context, fragment, view);
+        assert (pckName != null && !pckName.equals(""));
+        mappingFindView(target, source);
     }
 
-    /**
-     * @param holder RecyclerView.ViewHolder
-     * @param view   view的根节点
-     */
-    public static void mappingAdapter(Object holder, Object view) {
-        Object context = null;
-        try {
-            context = getMethod(view.getClass(), "getContext").invoke(view);
-        } catch (IllegalAccessException | InvocationTargetException e) {
-            e.printStackTrace();
-        }
-        assert Thread.currentThread().getName().equals("main");
-        assert context != null;
-        assert holder != null;
-        assert view != null;
-
-        mappingFindView(context, holder, view);
+    private static void mappingFindView(Object object) {
+        mappingFindView(object, object);
     }
 
-    /**
-     * @param context context
-     * @param target  持有各控件声明的类
-     * @param rootV   对应的 view 根节点
-     */
-    private static void mappingFindView(Object context, Object target, Object rootV) {
-        if (null == pkgName || pkgName.equals(""))
-            pkgName = getPackageName(context);
-        assert (pkgName != null && !pkgName.equals(""));
-        Class clz = target.getClass();
+    private static void mappingFindView(Object object, Object rootV) {
+        Class clz = object.getClass();
         Field[] allField = clz.getDeclaredFields();
 
         Field tmpF;
@@ -104,7 +99,7 @@ public class MappingFindView {
             for (int i = 0; i < allField.length; i++) {
                 tmpF = allField[i];
                 if (!isView(tmpF.getType())) {
-                    Log.i(TAG, i + " 不是继承自view 属性 跳过: name = " + tmpF.getName() + " type=" + tmpF.getType());
+//                    Log.i(TAG, i + " 不是继承自view 属性 跳过: name = " + tmpF.getName() + " type=" + tmpF.getType());
                     continue;
                 }
 
@@ -113,35 +108,18 @@ public class MappingFindView {
                 Object view = findViewById(rootV, tmpF.getName());
 
                 if (view == null) {
-                    Log.e(TAG, "没有找到该控件映射的id:控件名称" + tmpF.getName());
+//                    Log.e(TAG, "没有找到该控件映射的id:控件名称" + tmpF.getName());
                     continue;
                 }
 
-                tmpF.set(target, view);
+                tmpF.set(object, view);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    /**
-     * 根据Context获取包名
-     * @param content android.content
-     * @return 包名
-     * @throws InvocationTargetException
-     * @throws IllegalAccessException
-     */
-    private static String getPackageName(Object content) {
-        Method getPackageNameMethod = getMethod(content.getClass(), "getPackageName"); // 获取包名
-        if (getPackageNameMethod != null) {
-            try {
-                return (String) getPackageNameMethod.invoke(content); // 获取包名
-            } catch (IllegalAccessException | InvocationTargetException e) {
-                e.printStackTrace();
-            }
-        }
-        return null;
-    }
+
     /**
      * 从一个类中包括父类中，获取方法
      *
@@ -203,6 +181,15 @@ public class MappingFindView {
     }
 
 
+    private static String getPackName(Object activity) throws InvocationTargetException, IllegalAccessException {
+        Method getPackageNameMethod = getMethod(activity.getClass(), "getPackageName"); // 获取包名
+        if (getPackageNameMethod != null) {
+            return (String) getPackageNameMethod.invoke(activity); // 获取包名
+        }
+        return null;
+    }
+
+
     /**
      * 获取资源id,int值
      *
@@ -230,10 +217,10 @@ public class MappingFindView {
      */
     private static int getResourceID(Object object, String idName) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
         Method resourcesMethod = getMethod(object.getClass(), "getResources");
-        Object resources = resourcesMethod.invoke(object);
+             Object resources = resourcesMethod.invoke(object);
         Method getIdentifierMethod = resources.getClass().getMethod("getIdentifier", String.class, String.class, String.class);
 
-        return (int) getIdentifierMethod.invoke(resources, idName, "id", pkgName);
+        return (int) getIdentifierMethod.invoke(resources, idName, "id", pckName);
     }
 
     private static boolean isActivity(Class clz) {
